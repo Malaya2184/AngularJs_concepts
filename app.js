e454fe1e51014cc05a01b0f68c9b8110bd77e744
Malaya2184 <malayakumarswain8@gmail.com>
2021-07-15 03:31:20 +0530

    custom filter witn arguments

--- app.js
+++ app.js
@@ -24,24 +24,48 @@ $scope.upercase ="";
 //  }
 
 //using customFilter
+// $scope.sayFilteredMessage = function (name) {
+//     var msg = "hello loves likes likes likes ";
+    
+//     return (customFilter(msg+name));
+//  }
+
+//using customFilter with arguments
 $scope.sayFilteredMessage = function (name) {
     var msg = "hello loves likes likes likes ";
     
-    return (customFilter(msg+name));
+    return (customFilter(msg,'likes','loves'));
  }
 }
 //custom filter with no arguments * always define it outside the controller scope
-function customFilterFactorty() {
-    return function (input){
-        input = input || "";
-        //this syntax is to replace the 1st likes in to loves
-        // input = input.replace('likes', 'loves');
+    // function customFilterFactorty() {
+    //     return function (input){
+    //         input = input || "";
+    //         //this syntax is to replace the 1st likes in to loves
+    //         // input = input.replace('likes', 'loves');
 
-        //but this syntax replaces all likes to loves
-        input = input.replace(/likes/g, 'loves');
-        return input;
-    }
-}
+    //         //but this syntax replaces all likes to loves
+    //         input = input.replace(/likes/g, 'loves');
+    //         return input;
+    //     }
 
+
+    // }
+
+    //custom filter with arguments
+        function customFilterFactorty() {
+            return function (input, target, replace){
+                input = input || "";
+                //this syntax is to replace the 1st likes in to loves
+                // input = input.replace('likes', 'loves');
+
+                //but this syntax replaces all likes to loves
+                var regex = new RegExp(target, 'g');
+                input = input.replace(regex, replace);
+                //you can do the above line asbelow
+                // input= input.split(target).join(replace);
+                return input;
+            }
+        }
 
 })();
