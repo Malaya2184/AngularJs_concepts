a892d71e85e0cf7f196edcc7b0f7547bae6950ef
Malaya2184 <malayakumarswain8@gmail.com>
2021-07-16 02:36:50 +0530

    one way binding and watcher removal

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ angular.module("MyFirstApp", [])
 MyFirstController.$inject = ['$scope','$filter','$timeout'];
 
 function MyFirstController($scope,$filter, $timeout){
+$scope.firstname = "malaya";
 
+$scope.logfirstname= function () {
+    console.log($scope.firstname);
+}
+//when setfull name is calleed the watcher attached to full name unattached after the digestation cycle because of one way binding
+$scope.setfullname = function () {
+    
+    $scope.fullname = $scope.firstname + " kumar swain";
+}
+$scope.logfullname = function () {
+    console.log($scope.fullname);
+}
 }
 
 })();
