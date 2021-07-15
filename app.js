e84088b6973cf9cabcdbeb371a3f580adc2141c1
Malaya2184 <malayakumarswain8@gmail.com>
2021-07-16 02:41:07 +0530

    note added

--- app.js
+++ app.js
@@ -24,3 +24,6 @@ $scope.logfullname = function () {
 
 })();
 
+//note : must know how the watcher get removed after the calling of set full name
+// ans - because hele the full name is first instanciated inside the setname so after the function call watcher removed from it due to the one way binsing. if you instanciated the ful name before the function call the the full name will be the initial value and from starting the number of watchers will be one because it will not watch after the application start
+
