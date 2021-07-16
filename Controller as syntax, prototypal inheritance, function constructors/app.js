ef1a2fb06800b7aa61e83a805034866500f5ccc7
Malaya2184 <malayakumarswain8@gmail.com>
2021-07-17 02:35:50 +0530

    scope inheritance

---
+++ Controller as syntax, prototypal inheritance, function constructors/app.js
@@ -0,0 +1,55 @@
+(function(){
+    'use strict';
+   
+angular.module("MyFirstApp", [])
+
+
+.controller("MyFirstController",MyFirstController)
+.controller("MySecondController",MySecondController);
+MyFirstController.$inject = ['$scope','$filter','$timeout'];
+
+// function MyFirstController($scope,$filter, $timeout){
+// $scope.name = 'malaya';
+
+// }
+// function MySecondController($scope,$filter, $timeout){
+//     //try by commenting this line
+//     $scope.name = 'Raja';
+
+// }
+
+// function MyFirstController($scope,$filter, $timeout){
+//     this.name = 'malaya';
+    
+//     }
+//     function MySecondController($scope,$filter, $timeout){
+//         //try by commenting this line
+//         this.name = 'Raja';
+    
+
+function MyFirstController($scope,$filter, $timeout){
+    $scope.parentvalue = 1;
+    $scope.pc = this;
+    $scope.pc.parentvalue = 1;
+
+    }
+    MySecondController.$inject = ['$scope','$filter','$timeout'];
+    function MySecondController($scope,$filter, $timeout){
+        //try by commenting this line
+        console.log("$scope.parentvlue :", $scope.parentvalue);
+        console.log($scope);
+        // here the second controller's scope will mask the parent value of the first controller
+        $scope.parentvalue = 5;
+        console.log("$scope.parentvlue :", $scope.parentvalue);
+        console.log($scope);
+        console.log("$scope.pc.parentvlue :", $scope.pc.parentvalue);
+        $scope.pc.parentvalue = 5;
+        console.log("$scope.pc.parentvlue :", $scope.pc.parentvalue);
+        console.log("$scope.$parent.pc.parentvlue :", $scope.$parent.pc.parentvalue);
+        console.log($scope);
+    
+    }
+//     }
+
+})();
+
--- app.js
+++ app.js
@@ -4,50 +4,13 @@
 angular.module("MyFirstApp", [])
 
 
-.controller("MyFirstController",MyFirstController)
-.controller("MySecondController",MySecondController);
+.controller("MyFirstController",MyFirstController);
 MyFirstController.$inject = ['$scope','$filter','$timeout'];
-
-// function MyFirstController($scope,$filter, $timeout){
-// $scope.name = 'malaya';
-
-// }
-// function MySecondController($scope,$filter, $timeout){
-//     //try by commenting this line
-//     $scope.name = 'Raja';
-
-// }
-
-// function MyFirstController($scope,$filter, $timeout){
-//     this.name = 'malaya';
-    
-//     }
-//     function MySecondController($scope,$filter, $timeout){
-//         //try by commenting this line
-//         this.name = 'Raja';
     
 
 function MyFirstController($scope,$filter, $timeout){
-    $scope.parentvalue = 1;
-    $scope.pc = this;
-    $scope.pc.parentvalue = 1;
-
-    }
-    MySecondController.$inject = ['$scope','$filter','$timeout'];
-    function MySecondController($scope,$filter, $timeout){
-        //try by commenting this line
-        console.log("$scope.parentvlue :", $scope.parentvalue);
-        console.log($scope);
-        // here the second controller's scope will mask the parent value of the first controller
-        $scope.parentvalue = 5;
-        console.log("$scope.parentvlue :", $scope.parentvalue);
-        console.log($scope);
-        $scope.pc.parentvalue = 5;
-        console.log("$scope.parentvlue :", $scope.pc.parentvalue);
-        console.log($scope);
     
-    }
-//     }
 
+    }
 })();
 
