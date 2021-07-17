2a167388267228c32c6a91dbd1b94f00d45b24ee
Malaya2184 <malayakumarswain8@gmail.com>
2021-07-17 22:40:31 +0530

    custom service with function constructors

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 (function(){
-    'use strict';
+    // 'use strict';
    
 angular.module("MyFirstApp", [])
 
 
-.controller("MyFirstController",MyFirstController);
-MyFirstController.$inject = ['$scope','$filter','$timeout'];
+.controller("MyFirstController",MyFirstController)
+.controller("MySecondController",MySecondController)
+.service("shopinglistservice", shopinglistservice);
+//here 1st is the name of the service and second is the function responsible for the custom service . name will pass to the cntroller
     
-
-function MyFirstController($scope,$filter, $timeout){
-    
+MyFirstController.$inject = ['$scope','$filter','$timeout',"shopinglistservice"];
+function MyFirstController($scope,$filter, $timeout,shopinglistservice){
+    var ctrl1 = this;
+    ctrl1.itemname = "";
+    ctrl1.itemquantity = "";
+    ctrl1.additem = function(){
+        shopinglistservice.additem(ctrl1.itemname,ctrl1.itemquantity);
+        ctrl1.itemname = "";
+        ctrl1.itemquantity = "";
+    };
 
     }
+
+MySecondController.$inject = ['$scope','$filter','$timeout',"shopinglistservice"];
+function MySecondController($scope,$filter, $timeout,shopinglistservice){
+    var ctrl2= this;
+    ctrl2.items = shopinglistservice.getitem();
+        
+    
+        }
+
+//custom service function with function constructor
+function shopinglistservice() {
+    //function constructor
+    var service = this;
+    var items = [];
+    service.additem = function(itemname, itemquantity){
+        var item = {
+            itemname: itemname,
+            itemquantity: itemquantity
+        };
+        items.push(item);
+    };
+    service.getitem = function(){
+        
+        return items;
+    }
+    
+}
 })();
 
