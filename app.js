c1846cbf58399f62ea29394f98152a738b5213ce
Malaya2184 <malayakumarswain8@gmail.com>
2021-07-20 05:06:57 +0530

    http,call api, .constants,params

--- app.js
+++ app.js
@@ -5,148 +5,60 @@ angular.module("MyFirstApp", [])
 
 
 .controller("MyFirstController",MyFirstController)
-.service("shoppinglistService",shoppinglistService)
-.service("shoppingfilterservice",shoppingfilterservice);
+.service("menucategoryservie",menucategoryservie)
+.constant("ApiBasePath","http://davids-restaurant.herokuapp.com");
     
-MyFirstController.$inject = ['$scope','$filter','$timeout','shoppinglistService'];
-    function MyFirstController($scope,$filter, $timeout,shoppinglistService){
+MyFirstController.$inject = ["menucategoryservie"];
+    function MyFirstController(menucategoryservie){
         var ctrl1 = this;
-        ctrl1.itemname = "";
-        ctrl1.itemquantity = "";
-        ctrl1.items= shoppinglistService.getitem();
-        ctrl1.additem = function(){
-            shoppinglistService.additem(ctrl1.itemname, ctrl1.itemquantity);
-        }
-        ctrl1.removeitem = function(index){
-            shoppinglistService.removeitem(index);
+        var promise = menucategoryservie.getmenucategories();
+        promise.then(
+            function(response){
+                ctrl1.categories= response.data;
+            })
+            .catch(function(error){
+                console.log('something went wrong');
+            });
+        ctrl1.logmenuitems = function (shortname) {
+            var promise = menucategoryservie.getmenuforcategory(shortname);
+            promise.then(function(response){
+                console.log(response.data);
+            })
+            .catch(function(error){
+                console.log("error");
+            });
         }
 
 
         
     
         }
+menucategoryservie.$inject = ["$http","ApiBasePath"];
+function menucategoryservie($http,ApiBasePath){
 
-
-
-//declaration of service which will include in the factory functin
-//custom service function with function constructor
-//=======================================================
-//sync type and it can be written anathor type i.e below
-//comment this service and uncomment anthor to see the result
-//Simpler way to under stand the syntax part2 
-
-// shoppinglistService.$inject =['$q','shoppingfilterservice']
-// function shoppinglistService($q,shoppingfilterservice) {
-//     //function constructor
-//     var service = this;
-//     var items = [];
-//     service.additem = function(itemname, itemquantity){
-//         {
-//         var promise = shoppingfilterservice.checkname(itemname);
-//         promise.then(function(result){
-//             var nextpromise = shoppingfilterservice.checkquantity(itemquantity);
-
-//             nextpromise.then(function(result) {
-//                 var item = {
-//                     itemname: itemname,
-//                     itemquantity: itemquantity
-//                 };
-//                 items.push(item);
-//             }, function(error) {
-//                 alert('qunatity is > or ==5')
-//             });
-//         },function(error){
-//             alert("Not allowed to add cookies")
-//         });
-        
-//     }
-//     };
-//     service.getitem = function(){
-        
-//         return items;
-//     };
-//     service.removeitem = function(index){
-//         items.splice(index, 1);
-//     }
-    
-// }
-
-//=======================================================
-//anathor way
-//=======================================================
-shoppinglistService.$inject =['$q','shoppingfilterservice']
-function shoppinglistService($q,shoppingfilterservice) {
-    //function constructor
     var service = this;
-    var items = [];
-    service.additem = function(itemname, itemquantity){
-        {
-        var namepromise = shoppingfilterservice.checkname(itemname);
-        var quantitypromise = shoppingfilterservice.checkquantity(itemquantity);
-        $q.all([namepromise, quantitypromise])
-        .then(function(result){
-                var item = {
-                    itemname: itemname,
-                    itemquantity: itemquantity
-                };
-                items.push(item);
-        })
-        .catch(function(result){
-            alert("NOT ALLOWED")
+    service.getmenucategories = 
+    function()
+    {
+        var response = $http({
+            method : "GET",
+            url : (ApiBasePath + "/categories.json")
         });
-       
-        
+        return response;
     }
-    };
-    service.getitem = function(){
-        
-        return items;
-    };
-    service.removeitem = function(index){
-        items.splice(index, 1);
+    service.getmenuforcategory = 
+    function(shortname){
+        var response = $http({
+            method : "GET",
+            url : (ApiBasePath + "/menu_items.json"),
+            params : {
+                category : shortname
+            }
+        })
+        return response;
     }
-    
 }
-//=======================================================
-//=======================================================
-//sync type
-//Simpler way to under stand the syntax part2
-shoppingfilterservice.$inject = ['$q','$timeout']
-function shoppingfilterservice($q,$timeout)
-{
- var service = this;
- service.checkname = function(name){
-     var deffered = $q.defer();
-     var result = {
-         message:""
-     }
-        if(name.toLowerCase().indexOf('cookie') === -1){
-            deffered.resolve(result);
-        }
-        else{
-            deffered.reject(result);
-        }
-        return deffered.promise;
-
- }
- service.checkquantity = function(quantity){
-    var deffered = $q.defer();
-    var result = {
-        message:""
-    }
-       if(quantity<5){
-           deffered.resolve(result);
-       }
-       else{
-           deffered.reject(result);
-       }
-       return deffered.promise;
-
-}
-    
-}
-
-    
+  
 })();
 
 //note : this part is lil tricky and didfficult to under stand . so there is an alternative way to achieve this in part 3
